3b245f7326175a4d9ee9a38e435fa3387bdcc042
Tanasis Vlachopulos <tanas@tanas.eu>
2017-11-26 10:17:05 +0100

    disable models in main app; redo function index and waiting_device_api

--- EspHubServer/main/models.py
+++ EspHubServer/main/models.py
@@ -3,21 +3,21 @@ from django.db import models
 # from Presentation.WebUi.DataAccess import DBA, DAO
 from DataAccess import DBA, DAO
 
-class Device(DAO.Device):
-    @staticmethod
-    def get_all():
-        db = DBA.Dba("test.db")
-        return db.get_devices()
-
-    @staticmethod
-    def get(device_id):
-        db = DBA.Dba("test.db")
-        return db.get_device(device_id)
-
-
-class Record(DAO.Record):
-
-    @staticmethod
-    def get_all(device_id, limit=20):
-        db = DBA.Dba("test.db")
-        return db.get_record_from_device(device_id, limit=limit)
+# class Device(DAO.Device):
+#     @staticmethod
+#     def get_all():
+#         db = DBA.Dba("test.db")
+#         return db.get_devices()
+#
+#     @staticmethod
+#     def get(device_id):
+#         db = DBA.Dba("test.db")
+#         return db.get_device(device_id)
+#
+#
+# class Record(DAO.Record):
+#
+#     @staticmethod
+#     def get_all(device_id, limit=20):
+#         db = DBA.Dba("test.db")
+#         return db.get_record_from_device(device_id, limit=limit)
