11c0c6e39b8b6ce6ae79da4036fedf7f6d13ab14
Tanasis Vlachopulos <tanas@tanas.eu>
2017-12-05 00:56:32 +0100

    add experiments with ESP32

---
+++ EspHubLib/Esp32Experiments/GetChipID/GetChipID/GetChipID.ino
@@ -0,0 +1,16 @@
+uint64_t chipid;  
+
+void setup() {
+	Serial.begin(115200);
+}
+
+void loop() {
+	chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+	Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
+	Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
+
+  int int_chipid = ESP.getEfuseMac();
+  Serial.printf("int chip id: %d\n", int_chipid);
+	delay(3000);
+
+}
