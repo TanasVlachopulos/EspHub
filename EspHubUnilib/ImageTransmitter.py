3f88d29204b5f4bf381edb2f8b907147beaa9b3a
Tanasis Vlachopulos <tanas@tanas.eu>
2017-12-31 17:59:40 +0100

    enable basic com in Image Transitter

--- EspHubUnilib/ImageTransmitter.py
+++ EspHubUnilib/ImageTransmitter.py
@@ -1,7 +1,13 @@
+import json
+
 from Log import Log
 from MqttHandler import MqttHandler
 from Config import Config
 import uuid
+import time
+
+log = Log.get_logger()
+
 
 class ImageTransmitter(object):
 	def __init__(self):
@@ -9,10 +15,14 @@ class ImageTransmitter(object):
 		self.mqtt.register_topic("esp_hub/api/response/+", self.mqtt_api_response_callback)
 		self.request_queue = {}
 
-
 	def mqtt_api_response_callback(self, client, userdata, msg):
-		print(msg.payload.decode('utf-8'))
-		# todo decode and send to get_device_response_callback
+		response_id = msg.topic.split('/')[-1]
+		if response_id in self.request_queue:
+			response_callback = self.request_queue[response_id]
+			self.request_queue.pop(response_id)
+			response_callback(msg.payload.decode('utf-8'))
+		else:
+			print("unknown response id")
 
 	def get_devices(self):
 		"""
@@ -23,11 +33,23 @@ class ImageTransmitter(object):
 		self.request_queue[uid] = self.get_device_response_callback
 		self.mqtt.publish("esp_hub/api/request/{}".format(uid), "get_display_devices", qos=1)
 
-		# TODO wait for response
+		while uid in self.request_queue:
+			time.sleep(0.1)
 
 	def get_device_response_callback(self, payload):
 		# todo print output to console using click and return
-		pass
+		response = {}
+		try:
+			response = json.loads(payload)
+		except ValueError:
+			log.error("Cannot parse response from server.")
+			return
+
+		print("{:15}{:15}".format("device name", "device id"))
+		print("-"*30)
+		for device in response.get('payload', list()):
+			print("{:15}{:15}".format(device.get('name'), device.get('id')))
+
 
 
 if __name__ == "__main__":
