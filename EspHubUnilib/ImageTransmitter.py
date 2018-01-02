b0dda6d7d6b735c9b85cbd6b5afb49224085943d
Tanasis Vlachopulos <tanas@tanas.eu>
2018-01-02 23:30:51 +0100

    finalize steps 1, 2 and 3 in ImageTransmitter, issue #41

--- EspHubUnilib/ImageTransmitter.py
+++ EspHubUnilib/ImageTransmitter.py
@@ -16,8 +16,8 @@ class ImageTransmitter(object):
 	BASE_REQUEST_TOPIC = "esp_hub/api/request/"
 	BASE_RESPONSE_TOPIC = "esp_hub/api/response/"
 
-	def __init__(self, server_address, port=1883):
-		self.mqtt = MqttHandler(server_address, port)
+	def __init__(self, server_address, port=1883, user_name="", password="", client_id=""):
+		self.mqtt = MqttHandler(server_address, port, username=user_name, password=password, client_id=client_id)
 		self.request_pool = set()
 		self.response_pool = dict()
 
@@ -69,7 +69,7 @@ class ImageTransmitter(object):
 					for x in range(img.size[0]):
 						if img_data[x, y][3] < 255:
 							img_data[x, y] = (255, 255, 255, 255)
-				img.thumbnail([img.width, img.height], Image.ANTIALIAS)
+			# img.thumbnail([img.width, img.height], Image.ANTIALIAS)
 
 			img = img.convert('L')  # convert image to monochrome (white = 0xff, black = 0x00)
 			img_data = img.load()
@@ -107,58 +107,69 @@ class ImageTransmitter(object):
 
 
 @click.group()
-@click.option('-b', '--broker', type=str, required=True)
-@click.option('-p', '--port', type=int, default=1883)
+@click.option('-b', '--broker', type=str, required=True, help="MQTT broker domain name or IP address.")
+@click.option('-p', '--port', type=int, default=1883, help="Network port of MQTT broker. Default 1883.")
+@click.option('-u', '--user-name', type=str, default="", help="User name for connection to MQTT broker.")
+@click.option('-P', '--password', type=str, default="", help="Password for connection to MQTT broker.")
+@click.option('--client-id', type=str, default="", help="Specific client ID for connection to MQTT broker.")
 @click.pass_context
-def cli(ctx, broker, port):
-	ctx.obj = ImageTransmitter(broker, port)
+def cli(ctx, broker, port, user_name, password, client_id):
+	"""
+	This multi-tool provides several methods to send data to a devices which use EspHubLibrary. The MQTT protocol is used for data transmission so running MQTT broker is required.
+	"""
+	# TODO add verbose level
+	ctx.obj = ImageTransmitter(broker, port=port, user_name=user_name, password=password, client_id=client_id)
 
 
 @cli.command("send-image")
 @click.option('-d', '--device', type=str, required=True, help="Device ID or device name.")
-@click.option('--normalize/--no-normalize', default=True, help="Normalize image to monochrome format.")
+@click.option('--normalize/--no-normalize', default=True, help="Normalize image to monochrome format. Default Enabled.")
 @click.argument('bitmap', type=click.Path(exists=True, readable=True))
 @click.pass_obj
 def send_image(it, device, normalize, bitmap):
 	"""
+	Send single image to specific device.
 
-	:param it: Context to ImageTransmitter object
-	:type it: ImageTransmitter
-	:return:
+	Recommended format is monochrome .bmp with 1-bit color depth but also other format such as .png can be used if normalize option is enabled.
+	Normalization process try to convert image into standardized monochrome format but quality of results is questionable.
 	"""
 	bytes = it.convert_image_to_bytes(bitmap, normalize=normalize)
 	if bytes:
 		xbm_bytes = it.convert_bitmap_to_xbm_raw(bytes)
 		it.mqtt.publish(it.get_display_topic(device), xbm_bytes, qos=0)
 	else:
-		print('error')
+		log.error("Cannot convert image.")
 
 
 @cli.command("send-images")
 @click.option('-d', '--device', type=str, required=True, help="Device ID or device name.")
-@click.option('-f', '--frame-rate', type=int, default=10, help="Send n frames per second.")
+@click.option('-f', '--frame-rate', type=int, default=10, help="Send <INTEGER> frames per second. Default 10.")
+@click.option('--normalize/--no-normalize', default=True, help="Normalize image to monochrome format. Default Enabled.")
 @click.argument('bitmaps-folder', type=click.Path(exists=True))
 @click.pass_obj
-def send_images(it, device, frame_rate, bitmaps_folder):
+def send_images(it, device, frame_rate, normalize, bitmaps_folder):
 	"""
+	Send content of directory to specific device.
 
-	:param it:
-	:type it: ImageTransmitter
-	:param device:
-	:param frame_rate:
-	:param bitmaps_folder:
-	:return:
+	List all files in given directory and send it to display with specific frame-rate.
+
+	Recommended format is monochrome .bmp with 1-bit color depth but also other format such as .png can be used if normalize option is enabled.
+	Normalization process try to convert image into standardized monochrome format but quality of results is questionable.
 	"""
 	if not os.path.isdir(bitmaps_folder):
 		log.error("Given path '{}' is not a dictionary.".format(bitmaps_folder))
 		return
 
+	if frame_rate > 40:
+		log.error("Wowow calm down! {} FPS? Are you kidding? This is not for gaming monitor. Maximum is 40 FPS.".format(frame_rate))
+		return
+
 	converted_images = []
 
 	for file in [os.path.join(bitmaps_folder, f) for f in os.listdir(bitmaps_folder) if os.path.isfile(os.path.join(bitmaps_folder, f))]:
-		img_bytes = it.convert_image_to_bytes(file)
+		img_bytes = it.convert_image_to_bytes(file, normalize)
 		if img_bytes:
-			converted_images.append(it.convert_bitmap_to_xbm_raw(it.convert_bitmap_to_xbm_raw(img_bytes)))
+			converted_images.append(it.convert_bitmap_to_xbm_raw(img_bytes))
 
 	while True:
 		for img in converted_images:
@@ -168,11 +179,34 @@ def send_images(it, device, frame_rate, bitmaps_folder):
 
 
 @cli.command("pipe-interface")
-@click.option('--buffer', type=int, default=4096, help="Size of input buffer in bytes.")
+@click.option('--buffer', type=int, default=4096, help="Size of input buffer in bytes. Default 4096.")
 @click.option('-d', '--device', type=str, required=True, help="Device ID or device name.")
-@click.argument('pipe_path', type=click.Path(exists=False))
+@click.option('--normalize/--no-normalize', default=True, help="Normalize image to monochrome format. Default Enabled.")
+@click.argument('pipe_path', metavar="<PATH>", type=click.Path(exists=False))
 @click.pass_obj
-def pipe_interface(it, buffer, device, pipe_path):
+def pipe_interface(it, buffer, device, normalize, pipe_path):
+	"""
+	Create pipe interface for sending images.
+
+	CAUTION: This function is supported only from linux systems or in Linux subsystem on Windows. If you use Linux subsystem on Windows pipe path must be in Linux filesystem.
+
+	Command create named pipe in given <PATH> and listen in forever loop for incoming data. Data incoming into pipe are immediately send to given device.
+
+	If image content is bigger than given buffer data will be incomplete and image sending failed.
+
+	Recommended format is monochrome .bmp with 1-bit color depth but also other format such as .png can be used if normalize option is enabled.
+	Normalization process try to convert image into standardized monochrome format but quality of results is questionable.
+
+	EXAMPLE:
+
+	\b
+	In terminal 1 run:
+	py ImageTransmitter.py -b broker.eu pipe-interface -d 950316592 ~/mypipe
+
+	\b
+	Int terminal 2 send data:
+	cat image.bmp > ~/mypipe
+	"""
 	if os.name == 'nt':
 		log.error("This command is not supported on OS Windows.")
 		return
@@ -182,6 +216,10 @@ def pipe_interface(it, buffer, device, pipe_path):
 			os.mkfifo(pipe_path)
 	except AttributeError as e:
 		log.error("Cannot create named pipe.")
+		return
+	except PermissionError as e:
+		log.error("Cannot create named pipe. Permission denied.")
+		log.error(e)
 		return
 
 	pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
@@ -199,16 +237,11 @@ def pipe_interface(it, buffer, device, pipe_path):
 				log.info("Incoming data in pipe.")
 				bytes = os.read(fd, buffer)
 
-				try:
-					img_file = io.BytesIO(bytes)
-					img = Image.open(img_file)
-					img_bytes = img.tobytes()
-					xbm_bytes = it.convert_bitmap_to_xbm_raw(img_bytes)
-
-					it.mqtt.publish(it.get_display_topic(device), xbm_bytes, qos=0)
-				except OSError as e:
-					log.error("Invalid pipe input.")
-					log.error(e)
+				img_file = io.BytesIO(bytes)  # create in-memory binary file
+				img_bytes = it.convert_image_to_bytes(img_file, normalize)
+				if img_bytes:
+					xmb_bytes = it.convert_bitmap_to_xbm_raw(img_bytes)
+					it.mqtt.publish(it.get_display_topic(device), xmb_bytes, qos=0)
 
 			elif flags & select.POLLHUP:
 				# handle when other side close pipe for writing
@@ -222,8 +255,11 @@ def pipe_interface(it, buffer, device, pipe_path):
 @click.pass_obj
 def get_devices(it):
 	"""
-	Obtain list of display devices from EspHubServer over MQTT.
-	:return:
+	Obtain list of display devices.
+
+	CAUTION: For this command must be EspHubServer running and listening on same MQTT broker.
+
+	This command list all devices with connected display and show their IDs.
 	"""
 	uid = str(uuid.uuid4())
 	it.request_pool.add(uid)
@@ -241,4 +277,3 @@ def get_devices(it):
 
 if __name__ == "__main__":
 	cli()
-# ImageTransmitter.convert_image_to_bytes("num1.png", normalize=True)
