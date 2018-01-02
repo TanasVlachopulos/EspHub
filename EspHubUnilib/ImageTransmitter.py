1d945d5f3aab9c1281e5e2b1a3c1f6ce235a724a
Tanasis Vlachopulos <tanas@tanas.eu>
2018-01-02 01:34:28 +0100

    function version of sending image in unilib, close #33

--- EspHubUnilib/ImageTransmitter.py
+++ EspHubUnilib/ImageTransmitter.py
@@ -5,6 +5,7 @@ from Config import Config
 import uuid
 import time
 import click
+from PIL import Image
 
 log = Log.get_logger()
 
@@ -13,8 +14,8 @@ class ImageTransmitter(object):
 	BASE_REQUEST_TOPIC = "esp_hub/api/request/"
 	BASE_RESPONSE_TOPIC = "esp_hub/api/response/"
 
-	def __init__(self):
-		self.mqtt = MqttHandler("tanas.eu")
+	def __init__(self, server_address, port):
+		self.mqtt = MqttHandler(server_address, port)
 		self.request_pool = set()
 		self.response_pool = dict()
 
@@ -37,45 +38,54 @@ class ImageTransmitter(object):
 		else:
 			log.error("unknown response id")
 
-	# def get_device_response_callback(self, payload):
-	# 	# todo print output to console using click and return
-	# 	response = {}
-	# 	try:
-	# 		response = json.loads(payload)
-	# 	except ValueError:
-	# 		log.error("Cannot parse response from server.")
-	# 		return
-	#
-	# 	print("{:15}{:15}".format("device name", "device id"))
-	# 	print("-" * 30)
-	# 	for device in response.get('payload', list()):
-	# 		print("{:15}{:15}".format(device.get('name'), device.get('id')))
-
 	def wait_for_response(self, request_id):
 		while request_id not in self.response_pool:
 			time.sleep(0.1)
 
 		return self.response_pool[request_id]
 
+	def convert_bitmap_to_xbm_raw(self, bitmap_bytes):
+		xbm_lst = []
+		try:
+			for i in range(0, len(bitmap_bytes), 8):
+				# print(i, end=' ')
+				pixel_byte = 0
+				for bit in range(7, -1, -1):
+					pixel_byte <<= 1
+					pixel_byte |= bitmap_bytes[i + bit]
+
+				xbm_lst.append(pixel_byte)
+		except IndexError:
+			print('end')
+
+		return bytearray(xbm_lst)
+
 
 @click.group()
-@click.option('-b', type=str, required=True)
-@click.option('-p', type=int, default=1883)
+@click.option('-b', '--broker', type=str, required=True)
+@click.option('-p', '--port', type=int, default=1883)
 @click.pass_context
-def cli(ctx, b, p):
-	print(b)
-	ctx.obj = ImageTransmitter()
+def cli(ctx, broker, port):
+	ctx.obj = ImageTransmitter(broker, port)
+
 
 @cli.command("send-image")
+@click.option('-d', '--device', type=str, required=True, help="Device ID or device name.")
+@click.argument('bitmap', type=click.Path(exists=True, readable=True))
 @click.pass_obj
-def send_image(it):
+def send_image(it, device, bitmap):
 	"""
 
 	:param it: Context to ImageTransmitter object
 	:type it: ImageTransmitter
 	:return:
 	"""
-	click.echo(it.BASE_REQUEST_TOPIC)
+	img = Image.open(bitmap)
+	img_bytes = img.tobytes()
+	xbm_bytes = it.convert_bitmap_to_xbm_raw(img_bytes)
+
+	it.mqtt.publish("esp_hub/device/{}/display".format(device), xbm_bytes)
+
 
 @cli.command("get-devices")
 @click.pass_obj
@@ -96,6 +106,7 @@ def get_devices(it):
 		print("{:15}{:15}".format(device.get('name'), device.get('id')))
 
 	return response
+
 
 if __name__ == "__main__":
 	# it = ImageTransmitter()
