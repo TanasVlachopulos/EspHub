d9538230bc6b0a037ae22336dcef56b6f64ade04
Tanasis Vlachopulos <tanas@tanas.eu>
2018-02-08 21:34:06 +0100

    add dependency on sqlalchemy

--- EspHubServer/setup.py
+++ EspHubServer/setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='EspHubServer',
-    version='0.1',
-    author='Tanasis Vlachopulos',
-    # package_dir={'.': 'EspHubServer'},
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'Click',
-        'Django',
-        'paho-mqtt',
-        'Pillow',
-        'pygal',
-        'schedule',
-        'configparser',
-        # 'cairosvg',
-    ],
-    entry_points='''
+	name='EspHubServer',
+	version='0.1',
+	author='Tanasis Vlachopulos',
+	# package_dir={'.': 'EspHubServer'},
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[
+		'Click',
+		'Django',
+		'paho-mqtt',
+		'Pillow',
+		'pygal',
+		'schedule',
+		'configparser',
+		'sqlalchemy'
+		# 'cairosvg',
+	],
+	entry_points='''
         [console_scripts]
         esphub=EspHubServer.esphub:cli
     ''',
-        # tcmd=Plots.test:cli
+	# tcmd=Plots.test:cli
 )
