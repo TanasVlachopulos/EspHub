28be054cc77f9dca3a6c0b8137ff119cf18ed93b
Tanasis Vlachopulos <tanas@tanas.eu>
2018-03-10 22:45:37 +0100

    add task multiprocessing and test functionality

--- EspHubServer/DisplayBusiness/DisplayScheduledTask.py
+++ EspHubServer/DisplayBusiness/DisplayScheduledTask.py
@@ -1,7 +1,13 @@
+from Tools.Log import Log
+import time
+
+log = Log.get_logger()
+
 def display_scheduled_task(screen_id):
 	"""
 	Rund display scheduled task for screen with given ID.
 	:param screen_id:
 	:return:
 	"""
-	pass
+	log.info("running task for screen with id: {}".format(screen_id))
+	time.sleep(60)
--- EspHubServer/Scheduler/TaskScheduler.py
+++ EspHubServer/Scheduler/TaskScheduler.py
@@ -16,28 +16,50 @@ task_init_handlers = {
 }
 
 
-class TaskScheduler(object):
+class TaskScheduler(Process):
 	def __init__(self):
+		super().__init__()
 		self.tasks = []
 		self._next_task = None
+		self.end_event = Event()
+
+		self.init_scheduled_tasks()
+
+	def stop(self):
+		"""
+		Stop task processing and end task process.
+		:return:
+		"""
+		log.info("Terminating scheduled task processing.")
+		self.end_event.set()
 
 	def init_scheduled_tasks(self):
 		"""
 		Initialize tasks from database using type specific init functions.
 		:return:
 		"""
-		with DAC.keep_session() as db:
-			for task in DBA.get_active_tasks(db):
-				task_init_handler = task_init_handlers.get(task.type)
+		tasks = []
+		with DAC.keep_weak_session() as db:
+			tasks = DBA.get_active_tasks(db)
 
-				if task_init_handler:
-					self.tasks += task_init_handler(task.params)
-				else:
-					log.error("Unknown task init function for task type '{}'.".format(task.type))
+		for task in tasks:
+			task_init_handler = task_init_handlers.get(task.type)
+			task_group_id = task.group_id
 
-			for sch_task in self.tasks:
-				# schedule first run with random jitter (0.3-3 s) which prevent overlapping of task starts
-				sch_task.next_run = datetime.now() + timedelta(0, sch_task.interal) + (random.randint(300, 3000) / 1000)
+			if task_init_handler:
+				new_sch_tasks = task_init_handler(task.params)
+				# new_sch_tasks = []
+				for sch_task in new_sch_tasks:
+					sch_task.group_id = task_group_id  # add group ID to new ScheduledTask object
+				self.tasks += new_sch_tasks
+
+			else:
+				log.error("Unknown task init function for task type '{}'.".format(task.type))
+
+		for sch_task in self.tasks:
+			# schedule first run with random jitter (0.3-3 s) which prevent overlapping of task starts
+			sch_task.next_run = datetime.now() + timedelta(0, sch_task.interval) + timedelta(milliseconds=random.randint(300, 3000))
+			log.debug("Task '{} (G:{})' is scheduled on {}.".format(sch_task.task_type, sch_task.group_id, sch_task.next_run))
 
 	def _find_next_task(self):
 		"""
@@ -52,9 +74,12 @@ class TaskScheduler(object):
 		Calculate sleep time to nex task.
 		:return:
 		"""
-		sleep_time = self._next_task.next_run - datetime.now()
-		sleep_time_seconds = sleep_time.seconds + (sleep_time.microseconds / 1000000)
-		return sleep_time_seconds if sleep_time_seconds > 0 else 0
+		if self._next_task:
+			sleep_time = self._next_task.next_run - datetime.now()
+			sleep_time_seconds = sleep_time.seconds + (sleep_time.microseconds / 1000000)
+			return sleep_time_seconds if sleep_time_seconds > 0 else 0
+		else:
+			return 10  # if there is no task sleep 10s
 
 	def _get_task(self):
 		"""
@@ -68,32 +93,35 @@ class TaskScheduler(object):
 
 		if self._next_task.repeating:
 			self._next_task.next_run = datetime.now() + timedelta(0, self._next_task.interval)
+			log.debug("Task '{} (G:{})' is scheduled on {}.".format(self._next_task.task_type, self._next_task.group_id, self._next_task.next_run))
 		else:
 			self.tasks.remove(self._next_task)
 
 		return self._next_task
 
-	def run_forever(self):
+	def run(self):
 		"""
-		Start task processing.
+		Start task processing until end_event is set.
 		Each task are spawned in separate process.
-		:return: Event object. Processing task run until event is not set.
-		:type:
 		"""
-		event = Event()
-		while not event.is_set():
-			## TODO handle some end event and do this in separate process
+		while not self.end_event.is_set():
+			time.sleep(0.05)
 			self._find_next_task()
-			sleep = self._get_time_to_next_task()
-			time.sleep(sleep)
 			task = self._get_task()
 
-			process = Process(target=task.event, kwargs=task.kwargs,
-							  name="{} (G:{})".format(task.task_type, task.group_id))
-			process.start()
+			if task:
+				process = Process(target=task.event, kwargs=task.kwargs,
+								  name="{} (G:{})".format(task.task_type, task.group_id))
+				process.start()
 
+			# check and join running children
 			running_processes = active_children()
 			if len(running_processes) > 3:
 				log.warning(
 					"Too many running task processes: {} (count: {})".format([p.name for p in running_processes],
 																			 len(running_processes)))
+
+			sleep = self._get_time_to_next_task()
+			self.end_event.wait(sleep)  # wait until next task or undil end_event is set
+
+		log.debug("Scheduled task processing terminated.")
